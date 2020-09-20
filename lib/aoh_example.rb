9ada7a6ef4fe615cd28a5ba8d2fb9ec8a6acac11
Kishana Liburd <kishana.liburd@gmail.com>
2020-09-20 03:40:00 +0000

    Automatically backed up by Learn

--- lib/aoh_example.rb
+++ lib/aoh_example.rb
@@ -27,6 +27,7 @@ def aoh_lookup(aoh, row, key)
 end
 
 def aoh_update(aoh, row, key, new_value)
+  aoh[1][:name] = "Joell van Dyne (PGOAT)"
   # Update the AoH data at row and key to have the value of new_value
   # Return the updated AoH
 end
